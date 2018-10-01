b8650b32abaddfc06bc69adaec0582afe5b488f0
dasaexp <43750967+dasaexp@users.noreply.github.com>
2018-10-01 14:18:24 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt-add-repository -y ppa:rael-gc/rvm
 sudo apt-get remove ruby-full
 sudo apt-get autoremove
-sudo rm -rf /var/lib/ruby/2.5.0
+sudo rm -rf /var/lib/gems/2.5.0
 sudo apt-get update
 sudo apt-get install -y rvm
 
